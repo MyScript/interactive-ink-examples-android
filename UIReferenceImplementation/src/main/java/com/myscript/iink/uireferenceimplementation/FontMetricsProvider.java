38248d0321ce31b72c039e2c91692e1f071e6eea
MyScript <packages@myscript.com>
2020-06-17 11:16:27 +0200

    Optimize FontMetricsProvider by limiting use of getPrimaryHorizontal()

--- UIReferenceImplementation/src/main/java/com/myscript/iink/uireferenceimplementation/FontMetricsProvider.java
+++ UIReferenceImplementation/src/main/java/com/myscript/iink/uireferenceimplementation/FontMetricsProvider.java
@@ -11,6 +11,8 @@ import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.util.LruCache;
+import android.support.v4.util.Pair;
 import android.text.Layout;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -27,7 +29,6 @@ import com.myscript.iink.text.IFontMetricsProvider2;
 import com.myscript.iink.text.Text;
 import com.myscript.iink.text.TextSpan;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class FontMetricsProvider implements IFontMetricsProvider2
@@ -56,11 +57,16 @@ public class FontMetricsProvider implements IFontMetricsProvider2
     {
       if (obj == this)
         return true;
-      if (obj == null || !(obj instanceof FontKey))
+      if (!(obj instanceof FontKey))
         return false;
       FontKey other = (FontKey) obj;
       return style == other.style && size == other.size && family.equals(other.family);
     }
+  }
+
+  private interface IValueProvider<T>
+  {
+    T get();
   }
 
   private DisplayMetrics displayMetrics;
@@ -70,7 +76,8 @@ public class FontMetricsProvider implements IFontMetricsProvider2
   private RectF charBox = new RectF();
   private Map<String, Typeface> typefaceMap;
 
-  private Map<FontKey, Map<String, GlyphMetrics>> cache = new HashMap<>();
+  private final LruCache<Pair<FontKey, String>, GlyphMetrics> glyphMetricsCache = new LruCache<>(1024);
+  private final LruCache<Pair<FontKey, String>, Float> pairKerningCache = new LruCache<>(2048);
 
   public FontMetricsProvider(DisplayMetrics displayMetrics, Map<String, Typeface> typefaceMap)
   {
@@ -78,27 +85,27 @@ public class FontMetricsProvider implements IFontMetricsProvider2
     this.typefaceMap = typefaceMap;
   }
 
-  private final float x_mm2px(float mm)
+  private float x_mm2px(float mm)
   {
     return (mm / 25.4f) * displayMetrics.xdpi;
   }
 
-  private final float y_mm2px(float mm)
+  private float y_mm2px(float mm)
   {
     return (mm / 25.4f) * displayMetrics.ydpi;
   }
 
-  private final float x_px2mm(float px)
+  private float x_px2mm(float px)
   {
     return 25.4f * (px / displayMetrics.xdpi);
   }
 
-  private final float y_px2mm(float px)
+  private float y_px2mm(float px)
   {
     return 25.4f * (px / displayMetrics.ydpi);
   }
 
-  private final void updatePaint(int[] fontSizes, Typeface[] typefaces, int spanIndex)
+  private void updatePaint(int[] fontSizes, Typeface[] typefaces, int spanIndex)
   {
     paint.setTypeface(typefaces[spanIndex]);
     paint.setTextSize(fontSizes[spanIndex]);
@@ -120,68 +127,43 @@ public class FontMetricsProvider implements IFontMetricsProvider2
     return style.getFontSize() * displayMetrics.scaledDensity;
   }
 
-  private final float getRightSideBearing(String label, int start, int end, RectF charBox)
+  private GlyphMetrics getGlyphMetrics(FontKey fontKey, String glyph, IValueProvider<GlyphMetrics> valueProvider)
   {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+    Pair<FontKey, String> key = new Pair<>(fontKey, glyph);
+    synchronized (glyphMetricsCache)
     {
-      float advance = paint.getRunAdvance(label, start, end, start, end, false, end);
-      return x_px2mm(advance - charBox.right);
-    }
-    else
-    {
-      return 0;
+      GlyphMetrics metrics = glyphMetricsCache.get(key);
+      if (metrics == null)
+      {
+        metrics = valueProvider.get();
+        glyphMetricsCache.put(key, metrics);
+      }
+      return metrics;
     }
   }
 
-  private final GlyphMetrics getGlyphMetrics(FontKey fontKey, String label, int start, int end)
+  private float getPairKerning(FontKey fontKey, String glyph1, String glyph2, IValueProvider<Float> valueProvider)
   {
-    Map<String, GlyphMetrics> fontCache = cache.get(fontKey);
-    if (fontCache == null)
+    Pair<FontKey, String> key = new Pair<>(fontKey, glyph1 + glyph2);
+    synchronized (pairKerningCache)
     {
-      fontCache = new HashMap<>();
-      cache.put(fontKey, fontCache);
-    }
-    String glyph = label.substring(start, end);
-    GlyphMetrics metrics = fontCache.get(glyph);
-    if (metrics == null)
-    {
-      paint.getTextPath(label, start, end, 0, 0, charPath);
-      charPath.computeBounds(charBox, true);
-
-      // some glyphs paths may not be available (like for emojis)
-      // in that case we use simple text bounds, which are less precise but correct
-      if (charBox.isEmpty() && !label.equals(" "))
+      Float value = pairKerningCache.get(key);
+      if (value == null)
       {
-        Rect box = new Rect();
-        paint.getTextBounds(label, start, end, box);
-        charBox.left = box.left;
-        charBox.top = box.top;
-        charBox.right = box.right;
-        charBox.bottom = box.bottom;
+        value = valueProvider.get();
+        pairKerningCache.put(key, value);
       }
-
-      float x = x_px2mm(charBox.left);
-      float y = y_px2mm(charBox.top);
-      float width = x_px2mm(charBox.width());
-      float height = y_px2mm(charBox.height());
-
-      float leftSideBearing = -x;
-      float rightSideBearing = getRightSideBearing(label, start, end, charBox);
-
-      metrics = new GlyphMetrics(x, y, width, height, leftSideBearing, rightSideBearing);
-
-      fontCache.put(glyph, metrics);
+      return value;
     }
-    return metrics;
   }
 
   @Override
   public GlyphMetrics[] getGlyphMetrics(Text text, TextSpan[] spans)
   {
-    String label = text.getLabel();
+    final String label = text.getLabel();
 
     // Create spannable string that represent text with spans (ignoring color)
-    SpannableString string = new SpannableString(label);
+    final SpannableString string = new SpannableString(label);
 
     ColorStateList fontColor = ColorStateList.valueOf(Color.BLACK);
     ColorStateList fontLinkColor = null;
@@ -193,10 +175,10 @@ public class FontMetricsProvider implements IFontMetricsProvider2
     {
       Style style = spans[i].getStyle();
 
-      int typefaceStyle = FontUtils.getTypefaceStyle(style);
       String fontFamily = style.getFontFamily();
-      int fontSize = Math.round(y_mm2px(style.getFontSize()));
 
+      int typefaceStyle = FontUtils.getTypefaceStyle(style);
+      int fontSize = Math.round(y_mm2px(style.getFontSize()));
       int start = text.getGlyphBeginAt(spans[i].getBeginPosition());
       int end = text.getGlyphEndAt(spans[i].getEndPosition() - 1);
 
@@ -217,43 +199,100 @@ public class FontMetricsProvider implements IFontMetricsProvider2
     GlyphMetrics[] charBoxes = new GlyphMetrics[glyphCount];
 
     // Layout text
-    if (glyphCount > 1)
+    int spanEnd = -1;
+    int spanIndex = -1;
+    FontKey fontKey = null;
+    float pos = 0;
+    String glyph_ = null;
+    final Layout[] layout = new Layout[1];
+    for (int i = 0; i < glyphCount; ++i)
     {
-      Layout layout = new StaticLayout(string, paint_, 100000, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
-      if (layout.getLineCount() != 1)
-        throw new RuntimeException();
-
-      int spanEnd = -1;
-      int spanIndex = -1;
-      FontKey fontKey = null;
-      for (int i = 0; i < glyphCount; ++i)
+      if (i >= spanEnd)
       {
-        if (i >= spanEnd)
-        {
-          ++spanIndex;
-          fontKey = new FontKey(spans[spanIndex].getStyle().getFontFamily(), typefaces[spanIndex].getStyle(), fontSizes[spanIndex]);
-          spanEnd = spans[spanIndex].getEndPosition();
-          updatePaint(fontSizes, typefaces, spanIndex);
-        }
-
-        int start = text.getGlyphBeginAt(i);
-        int end = text.getGlyphEndAt(i);
-        GlyphMetrics m = getGlyphMetrics(fontKey, label, start, end);
-
-        float posPx = layout.getPrimaryHorizontal(start);
-        float pos = x_px2mm(posPx);
-
-        charBoxes[i] = new GlyphMetrics(pos + m.boundingBox.x, m.boundingBox.y, m.boundingBox.width, m.boundingBox.height, m.leftSideBearing, m.rightSideBearing);
+        ++spanIndex;
+        fontKey = new FontKey(spans[spanIndex].getStyle().getFontFamily(), typefaces[spanIndex].getStyle(), fontSizes[spanIndex]);
+        spanEnd = spans[spanIndex].getEndPosition();
+        updatePaint(fontSizes, typefaces, spanIndex);
       }
-    }
-    else
-    {
-      FontKey fontKey = new FontKey(spans[0].getStyle().getFontFamily(), typefaces[0].getStyle(), fontSizes[0]);
-      updatePaint(fontSizes, typefaces, 0);
-      int start = text.getGlyphBeginAt(0);
-      int end = text.getGlyphEndAt(0);
-      GlyphMetrics m = getGlyphMetrics(fontKey, label, start, end);
-      charBoxes[0] = new GlyphMetrics(m.boundingBox.x, m.boundingBox.y, m.boundingBox.width, m.boundingBox.height, m.leftSideBearing, m.rightSideBearing);
+
+      final int start = text.getGlyphBeginAt(i);
+      final int end = text.getGlyphEndAt(i);
+      String glyph = label.substring(start, end);
+      GlyphMetrics m = getGlyphMetrics(fontKey, glyph, new IValueProvider<GlyphMetrics>()
+      {
+        @Override
+        public GlyphMetrics get()
+        {
+          paint.getTextPath(label, start, end, 0, 0, charPath);
+          charPath.computeBounds(charBox, true);
+
+          // some glyphs paths may not be available (like for emojis)
+          // in that case we use simple text bounds, which are less precise but correct
+          if (charBox.isEmpty() && !label.equals(" "))
+          {
+            Rect box = new Rect();
+            paint.getTextBounds(label, start, end, box);
+            charBox.left = box.left;
+            charBox.top = box.top;
+            charBox.right = box.right;
+            charBox.bottom = box.bottom;
+          }
+
+          float x = x_px2mm(charBox.left);
+          float y = y_px2mm(charBox.top);
+          float width = x_px2mm(charBox.width());
+          float height = y_px2mm(charBox.height());
+
+          float leftSideBearing = -x;
+          float rightSideBearing;
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+          {
+            float advance = paint.getRunAdvance(label, start, end, start, end, false, end);
+            rightSideBearing = x_px2mm(advance - charBox.right);
+          }
+          else
+          {
+            rightSideBearing = 0; // expect degraded reflow of typeset text
+          }
+
+          return new GlyphMetrics(x, y, width, height, leftSideBearing, rightSideBearing);
+        }
+      });
+
+      if (i > 0)
+      {
+        float pairKerning;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && (" ".equals(glyph_) || " ".equals(glyph)))
+        {
+          pairKerning = 0; // pair kerning is zero for space, when right side bearing is correct
+        }
+        else
+        {
+          final float defaultPos = pos;
+          pairKerning = getPairKerning(fontKey, glyph_, glyph, new IValueProvider<Float>()
+          {
+            @Override
+            public Float get()
+            {
+              if (layout[0] == null)
+              {
+                layout[0] = new StaticLayout(string, paint_, 100000, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                if (layout[0].getLineCount() != 1)
+                  throw new RuntimeException();
+              }
+              float posPx = layout[0].getPrimaryHorizontal(start);
+              float pos = x_px2mm(posPx);
+              return pos - defaultPos;
+            }
+          });
+        }
+        pos += pairKerning;
+      }
+
+      charBoxes[i] = new GlyphMetrics(pos + m.boundingBox.x, m.boundingBox.y, m.boundingBox.width, m.boundingBox.height, m.leftSideBearing, m.rightSideBearing);
+
+      glyph_ = glyph;
+      pos += m.boundingBox.width - m.leftSideBearing + m.rightSideBearing;
     }
 
     return charBoxes;
