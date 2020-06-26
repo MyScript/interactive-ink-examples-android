8f79850bcb17d6fa91c2b08ea0f5663714448412
MyScript <packages@myscript.com>
2020-06-26 13:38:00 +0200

    Allow more paste use cases in Demo example

--- Demo/src/main/java/com/myscript/iink/demo/MainActivity.java
+++ Demo/src/main/java/com/myscript/iink/demo/MainActivity.java
@@ -475,7 +475,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final boolean displayAddImage = false; // hasTypes && isRoot;
     final boolean displayRemove = !isRoot;
     final boolean displayCopy = !onTextDocument || !isRoot;
-    final boolean displayPaste = hasTypes && isRoot;
+    final boolean displayPaste = isRoot;
     final boolean displayImport = false; // hasImports;
     final boolean displayExport = false; // hasExports;
 
