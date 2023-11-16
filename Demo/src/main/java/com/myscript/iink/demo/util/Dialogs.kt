7d2c1c9936d3f6e2e6fa90347c4b822629de6c0b
MyScript <packages@myscript.com>
2023-11-16 11:09:12 +0100

    More native experience for prediction dialog settings

--- Demo/src/main/java/com/myscript/iink/demo/util/Dialogs.kt
+++ Demo/src/main/java/com/myscript/iink/demo/util/Dialogs.kt
@@ -6,8 +6,11 @@ import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.EditText
-import android.widget.Switch
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
 import androidx.annotation.StringRes
+import androidx.appcompat.widget.SwitchCompat
 import com.myscript.iink.demo.R
 
 fun Context.launchSingleChoiceDialog(
@@ -51,16 +54,16 @@ fun Context.launchTextBlockInputDialog(onInputDone: (text: String) -> Unit) {
     val editTextLayout = LayoutInflater.from(this).inflate(R.layout.editor_text_input_layout, null)
     val editText = editTextLayout.findViewById<EditText>(R.id.editor_text_input)
     val builder = AlertDialog.Builder(this)
-            .setView(editTextLayout)
-            .setTitle(R.string.editor_dialog_insert_text_title)
-            .setPositiveButton(R.string.editor_dialog_insert_text_action) { _, _ ->
-                val text = editText.text.toString()
-                if (text.isNotBlank()) {
-                    onInputDone(text)
-                }
+        .setView(editTextLayout)
+        .setTitle(R.string.editor_dialog_insert_text_title)
+        .setPositiveButton(R.string.editor_dialog_insert_text_action) { _, _ ->
+            val text = editText.text.toString()
+            if (text.isNotBlank()) {
+                onInputDone(text)
             }
-            .setNegativeButton(R.string.dialog_cancel, null)
-            .create()
+        }
+        .setNegativeButton(R.string.dialog_cancel, null)
+        .create()
     editText.requestFocus()
     builder.show()
 }
@@ -71,20 +74,32 @@ fun Context.launchPredictionDialog(
         onInputDone: (Boolean, Int) -> Unit
 ) {
     val editPredictionLayout = LayoutInflater.from(this).inflate(R.layout.editor_prediction_layout, null)
-    val switchEnable = editPredictionLayout.findViewById<Switch>(R.id.switch_enable)
-    val numberDuration = editPredictionLayout.findViewById<EditText>(R.id.number_duration)
+    val enablePredictionSwitch = editPredictionLayout.findViewById<SwitchCompat>(R.id.editor_dialog_prediction_toggle)
+    val durationLabel = editPredictionLayout.findViewById<TextView>(R.id.editor_dialog_prediction_duration_label)
+    durationLabel.text = getString(R.string.editor_dialog_prediction_duration, durationMs)
+    val durationSeekBar = editPredictionLayout.findViewById<SeekBar>(R.id.editor_dialog_prediction_duration).apply {
+        setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    durationLabel.text = getString(R.string.editor_dialog_prediction_duration, seekBar?.progress ?: 0)
+                }
+            }
 
-    switchEnable.isChecked = enabled
-    numberDuration.setText(durationMs.toString())
+            override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
+            override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
+        })
+    }
+
+    enablePredictionSwitch.isChecked = enabled
+    durationSeekBar.progress = durationMs
 
     AlertDialog.Builder(this)
-            .setView(editPredictionLayout)
-            .setTitle(R.string.editor_dialog_prediction_title)
-            .setPositiveButton(R.string.dialog_ok) { _, _ ->
-                val durationString = numberDuration.text.toString()
-                if (durationString.isNotEmpty())
-                    onInputDone(switchEnable.isChecked, durationString.toInt())
-            }
-            .setNegativeButton(R.string.dialog_cancel, null)
-            .show()
+        .setView(editPredictionLayout)
+        .setTitle(R.string.editor_dialog_prediction_title)
+        .setPositiveButton(R.string.dialog_ok) { _, _ ->
+            durationSeekBar.setOnSeekBarChangeListener(null)
+            onInputDone(enablePredictionSwitch.isChecked, durationSeekBar.progress)
+        }
+        .setNegativeButton(R.string.dialog_cancel, null)
+        .show()
 }
